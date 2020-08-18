97d52bfac15679e3dc7de2cd656e0e1b8987dc86
Natalia Bannikova <natabannikova10@gmail.com>
2020-08-18 11:33:24 +0300

    from hover to jquery

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(function(){
 
-
+$('.helicopter').hover(
+    function(){
+        $('.helic-img').addClass('helicopter-on').removeClass('helicopter-off');
+    },
+    function(){
+        $('.helic-img').removeClass('helicopter-on').addClass('helicopter-off');
+    }
+);
 
 
 });
