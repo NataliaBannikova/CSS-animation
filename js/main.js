daf2dbe2febf5356f616bfcca2980e4cb86dfe15
Natalia Bannikova <natabannikova10@gmail.com>
2020-08-18 13:02:37 +0300

    animations optimization

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ $(function(){
 $('.helicopter').hover(
     function(){
         $('.helic-img').addClass('helicopter-on').removeClass('helicopter-off');
+        $('.helicopter').addClass('animation-on').removeClass('animation-off');
     },
     function(){
         $('.helic-img').removeClass('helicopter-on').addClass('helicopter-off');
+        $('.helicopter').removeClass('animation-on').addClass('animation-off');
     }
 );
 
